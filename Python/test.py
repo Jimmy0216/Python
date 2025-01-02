30af79dc353764f904faf6119d760afd2cd3becb
Jaemin Lee <maximin910216@gmail.com>
2025-01-02 19:27:16 +0900

    모듈화

---
+++ Python/test.py
@@ -0,0 +1,12 @@
+import calc
+print(calc.add(3, 7))
+
+# from calc import *
+
+# print(basic.add(3, 7))
+# print(advanced.mul(3,7))
+
+# import calc
+
+# print(calc.basic.add(3,7))
+# print(calc.advanced.div(3, 7))
